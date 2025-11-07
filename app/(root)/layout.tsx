d5064da1d0e389ffa2d7b595852266e6abd73494
parvgoel2706 <parvgoel2345@gmail.com>
2025-11-07 20:24:48 +0530

    Platform name

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,8 +13,8 @@ const Layout = async ({ children }: { children: ReactNode }) => {
     <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <Image src="/logo.svg" alt="Mock2Hire Logo" width={38} height={32} />
+          <h2 className="text-primary-100">Mock2Hire</h2>
         </Link>
       </nav>
 
